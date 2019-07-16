0e8ccf8baafcc10fc29514164b9e48e01e78994d
Alex Ubilla <ajubilla@espol.edu.ec>
2019-07-16 06:21:34 -0500

    new

--- crud/tests.py
+++ crud/tests.py
@@ -24,8 +24,11 @@ class BookTestCase(TestCase):
 
         self.assertEqual(lista_libros+1, lista_libros_actualizado)
 
+
     # def test_editar_libro(self):
-    #   pass
+    #   pass  
+
+
 
     # def test_eliminar_libro(self):
     #   pass
@@ -46,17 +49,26 @@ class ViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
-    # def test_create_view(self):
-    #   pass
+    def test_create_view(self):
+        response = self.client.get(reverse('create'), {'title':'new', 'price':50,'author':'alex'})
+        self.assertEqual(response.status_code, 302)
 
-    # def test_add_view(self):
-    #   pass
+    def test_add_view(self):
+        response = self.client.get(reverse('add_book'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_book.html')
 
-    # def test_delete_view(self):
-    #   pass
+    def test_update_view(self):
+        response = self.client.get(reverse('/update/1'))
+        self.assertEqual(response.status_code, 301)
 
-    # def test_edit_view(self):
-    #   pass
+    def test_delete_view(self):
+        response = self.client.get(reverse('/delete/1'))
+        self.assertEqual(response.status_code, 301)
+
+    def test_edit_view(self):
+        response = self.client.get(reverse('/edit/1'))
+        self.assertEqual(response.status_code, 301)
 
 
 class FunctionsTestCase(TestCase):
@@ -79,4 +91,33 @@ class FunctionsTestCase(TestCase):
         libros = BookList.objects.all()
         msj = agregarLibroAlCarrito(libros[0], carrito)
         msj_esperado = 'Libro: Fire & Ice fue agregado al carrito'
+        self.assertEqual(msj_esperado, msj)
+
+    def test_agregar_carrito_nuevo(self):
+        carrito = []
+        libros = BookList.objects.all()
+        libro_prueba = BookList.objects.create(title="Fire",
+                                               price=90,
+                                               author="Alex")
+        msj = agregarLibroAlCarrito(libros[0], carrito)
+        msj_esperado = 'Libro: '+(libro_prueba.title)+' fue agregado al carrito'
+        self.assertEqual(msj_esperado, msj)
+
+    def test_agregar_carrito_else_libro(self):
+        carrito = []
+        libros = BookList.objects.all()
+        libro_prueba = None
+        msj = agregarLibroAlCarrito(lista_prueba, carrito)
+        msj_esperado = 'Error: No hay ningun libro'
+        self.assertEqual(msj_esperado, msj)
+
+    def test_agregar_carrito_libro_maximo(self):
+        carrito = []
+        libros = BookList.objects.all()
+        libro_prueba = BookList.objects.create(title="Fire",
+                                               price=90,
+                                               author="Alex")
+        for i in range(10)
+        msj = agregarLibroAlCarrito(lista_prueba, carrito)
+        msj_esperado = 'Solo puede ingresar hasta un maximo de 10 Libros al carrito'
         self.assertEqual(msj_esperado, msj)
