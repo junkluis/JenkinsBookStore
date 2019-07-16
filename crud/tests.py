e997b27363ef5315e3bc7e988e4c576ed2f9da7c
luis zuniga <lufezuro@gmail.com>
2019-07-16 00:37:11 -0500

    pep8 corrections

--- crud/tests.py
+++ crud/tests.py
@@ -4,24 +4,25 @@ from .models import BookList
 from .views import *
 
 # Create your tests here.
+
 class BookTestCase(TestCase):
 
     def setUp(self):
-        #Creamos un libro para las pruebas
-        BookList.objects.create(title="Fire & Ice", price=90, author="Luis Zuniga")
-
+        # Creamos un libro para las pruebas
+        BookList.objects.create(title="Fire & Ice",
+                                price=90,
+                                author="Luis Zuniga")
 
     def test_crear_nuevo_libro(self):
 
         lista_libros = len(BookList.objects.all())
         info_libro = ["Festin de Cuervos", 40, "Luis Zuniga"]
-        BookList.objects.create( title=info_libro[0],
-                                 price=info_libro[1],
-                                 author=info_libro[2])
+        BookList.objects.create(title=info_libro[0],
+                                price=info_libro[1],
+                                author=info_libro[2])
         lista_libros_actualizado = len(BookList.objects.all())
 
-        self.assertEqual(lista_libros+1 , lista_libros_actualizado)
-
+        self.assertEqual(lista_libros+1, lista_libros_actualizado)
 
     # def test_editar_libro(self):
     #   pass
@@ -36,12 +37,11 @@ class BookTestCase(TestCase):
     #   pass
 
 
-
 class ViewsTestCase(TestCase):
 
-    #Prueba de una vista.
+    # Prueba de una vista.
     def test_index_view(self):
-        #response = self.client.get(reverse('index', args=[self.userName]))
+        # response = self.client.get(reverse('index', args=[self.userName]))
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
@@ -59,23 +59,24 @@ class ViewsTestCase(TestCase):
     #   pass
 
 
-
 class FunctionsTestCase(TestCase):
 
-
     def setUp(self):
-        #Creamos un libro para las pruebas
-        libro_prueba = BookList.objects.create(title="Fire & Ice", price=90, author="Luis Zuniga")
-        libro_prueba = BookList.objects.create(title="Fire & Ice II", price=80, author="Luis Zuniga")
-        libro_prueba = BookList.objects.create(title="Fire & Ice III", price=40, author="Luis Zuniga")
+        # Creamos un libro para las pruebas
+        libro_prueba = BookList.objects.create(title="Fire & Ice",
+                                               price=90,
+                                               author="Luis Zuniga")
+        libro_prueba = BookList.objects.create(title="Fire & Ice II",
+                                               price=80,
+                                               author="Luis Zuniga")
+        libro_prueba = BookList.objects.create(title="Fire & Ice III",
+                                               price=40,
+                                               author="Luis Zuniga")
 
-
-    #Prueba de una vista.
+    # Prueba de una vista.
     def test_agregar_carrito(self):
         carrito = []
         libros = BookList.objects.all()
         msj = agregarLibroAlCarrito(libros[0], carrito)
         msj_esperado = 'Libro: Fire & Ice fue agregado al carrito'
         self.assertEqual(msj_esperado, msj)
-        
-
