f9a5214060e554d9c084cf3d634c045b300337a3
Alfonso Pulido Carrera <32580083+alfonsopulido13@users.noreply.github.com>
2019-07-16 10:56:03 -0500

    taller cobertura

--- crud/tests.py
+++ crud/tests.py
@@ -24,6 +24,17 @@ class BookTestCase(TestCase):
 
         self.assertEqual(lista_libros+1, lista_libros_actualizado)
 
+    def test_crear_nuevo_libro_2(self):
+
+        lista_libros = len(BookList.objects.all())
+        info_libro = ["Mitos nordicos", 100 , "Neil Gaiman"]
+        BookList.objects.create(title=info_libro[0],
+                                price=info_libro[1],
+                                author=info_libro[2])
+        lista_libros_actualizado = len(BookList.objects.all())
+
+        self.assertEqual(lista_libros+1, lista_libros_actualizado)
+
     # def test_editar_libro(self):
     #   pass
 
@@ -45,6 +56,12 @@ class ViewsTestCase(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_index_view_2(self):
+            # response = self.client.get(reverse('index', args=[self.userName]))
+            response = self.client.get(reverse('index2'))
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'index2.html')
 
     # def test_create_view(self):
     #   pass
@@ -72,6 +89,10 @@ class FunctionsTestCase(TestCase):
         libro_prueba = BookList.objects.create(title="Fire & Ice III",
                                                price=40,
                                                author="Luis Zuniga")
+        libro_prueba = BookList.objects.create(title="Mitos nordicos",
+                                               price=50,
+                                               author="Neil Gaiman")
+
 
     # Prueba de una vista.
     def test_agregar_carrito(self):
@@ -80,3 +101,11 @@ class FunctionsTestCase(TestCase):
         msj = agregarLibroAlCarrito(libros[0], carrito)
         msj_esperado = 'Libro: Fire & Ice fue agregado al carrito'
         self.assertEqual(msj_esperado, msj)
+
+    def test_agregar_carrito_2(self):
+        carrito = []
+        libros = BookList.objects.all()
+        msj = agregarLibroAlCarrito(libros[0], carrito)
+        msj_esperado = 'Libro: Mitos Nordicos fue agregado al carrito'
+        self.assertEqual(msj_esperado, msj)
+
