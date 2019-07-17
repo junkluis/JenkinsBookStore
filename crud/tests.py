0c8b9c4d9aa1ff6387c4dc4b1b82542a4aa4c802
AnibalG <aagamboa@espol.edu.ec>
2019-07-16 19:48:09 -0500

    more tests

--- crud/tests.py
+++ crud/tests.py
@@ -26,8 +26,8 @@ class BookTestCase(TestCase):
 
     def test_editar_libro(self):
         BookList.objects.filter(title="Fire & Ice").update(price=50)
-        book = BookList.objects.get("Fire & Ice")
-        self.assertEqual(50, book.price)
+        libro = BookList.objects.get(title="Fire & Ice")
+        self.assertEqual(50, libro.price)
 
     def test_eliminar_libro(self):
         lista_libros = len(BookList.objects.all())
@@ -36,8 +36,11 @@ class BookTestCase(TestCase):
 
         self.assertEqual(lista_libros-1, lista_libros_actualizado)
 
-    # def test_buscar_libro(self):
-    #   pass
+    def test_buscar_libro(self):
+        titulo_libro_a_buscar = "Fire & Ice"
+        libro = BookList.objects.get(title=titulo_libro_a_buscar)
+
+        self.assertEqual(libro.title, titulo_libro_a_buscar)
 
     # def test_libro_sin_precio(self):
     #   pass
@@ -53,13 +56,16 @@ class ViewsTestCase(TestCase):
         self.assertTemplateUsed(response, 'index.html')
 
     def test_create_view(self):
-        response = self.client.get('create')
+        form_data = {"title": "1984", "price": 20, "author": "George Orwell"}
+        response = self.client.get(reverse('create'), form_data)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'add_book.html')
 
-    # def test_add_view(self):
-    #   pass
+    def test_add_view(self):
+        response = self.client.get(reverse('add_book'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_book.html')
 
     # def test_delete_view(self):
     #   pass
