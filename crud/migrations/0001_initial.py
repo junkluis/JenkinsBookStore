2ecc9a11f7f2347157b32b92ce616e8f0c6e3a34
wmartine32 <wellinmart32@gmail.com>
2019-08-25 14:13:11 -0500

    Realizando pruebas pep8

--- crud/migrations/0001_initial.py
+++ crud/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='BookList',
             fields=[
-                ('id', models.AutoField(auto_created=True, 
+                ('id', models.AutoField(auto_created=True,
                                         primary_key=True,
                                         serialize=False,
                                         verbose_name='ID')),
