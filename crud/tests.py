a9037c5cabd5e74382647dd880edf50e65d3c687
wmartine32 <wellinmart32@gmail.com>
2019-08-25 14:29:17 -0500

    Realizando pruebas pep8

--- crud/tests.py
+++ crud/tests.py
@@ -136,7 +136,7 @@ class FunctionsTestCase(TestCase):
     def test_agregar_carrito_exceso(self):
         car = []
         book = BookList.objects.first()
-        x = 'Solo puede ingresar hasta un maximo de 10 Libros' + 
+        x = 'Solo puede ingresar hasta un maximo de 10 Libros' +  
             ' al carrito'
         for i in range(15):
             msj = agregarLibroAlCarrito(book, car)
